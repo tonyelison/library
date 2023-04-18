67db52d10e7e20903de245a8bd1edf0b0e66c647
Anthony Elison <tonyelison37@gmail.com>
2023-04-18 12:50:33 -0600

    Display simple list of book titles via js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = ['book1', 'book2', 'book3'];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -10,3 +12,15 @@ Book.prototype.info = function () {
     this.read ? 'was read' : 'not read yet'
   }`;
 };
+
+function displayBooks() {
+  const bookList = document.querySelector('.book-list');
+  myLibrary.forEach((book) => {
+    const card = document.createElement('div');
+    card.classList.add('book');
+    card.textContent = book;
+    bookList.appendChild(card);
+  });
+}
+
+displayBooks();
